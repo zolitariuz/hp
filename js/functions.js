f3821ef2c83918dddcc8099d04a043ff833ad53e
raul <raul@zamacona.me>
2013-09-22 22:37:38 -0500

    más response y sección de segmentos

--- js/functions.js
+++ js/functions.js
@@ -6,31 +6,42 @@
 	muestraInfo("aplicaciones");
 
 	// detecta que opción del menú izquierdo fue "clickeada" y cambia la info
-	menuIzq.addEventListener("click", cambiaTab, false);
+	if ( menuIzq != null ){
+		menuIzq.addEventListener("click", cambiaTab, false);
+	}
 
 }()); // Immediately-Invoked Function Expression (IIFE)
 
 function resetInfo() {
 // Esconde la info y le quita el estado de activo al menu
 	var ap = document.getElementById("aplicaciones");
-	ap.style.display = "none";
-	var apLink = document.getElementById("aplicacionesLink");
-	apLink.className = "";
+	if ( ap != null ){
+		console.log('s');
+		ap.style.display = "none";
+		var apLink = document.getElementById("aplicacionesLink");
+		apLink.className = "";
+	}
 
 	var so = document.getElementById("soluciones");
-	so.style.display = "none";
-	var soLink = document.getElementById("solucionesLink");
-	soLink.className = "";
+	if ( so != null ){
+		so.style.display = "none";
+		var soLink = document.getElementById("solucionesLink");
+		soLink.className = "";
+	}
 
 	var te = document.getElementById("tendencias");
-	te.style.display = "none";
-	teLink = document.getElementById("tendenciasLink");
-	teLink.className = "";
+	if ( te != null ){
+		te.style.display = "none";
+		teLink = document.getElementById("tendenciasLink");
+		teLink.className = "";
+	}
 
 	var vi = document.getElementById("videos");
-	vi.style.display = "none";
-	viLink = document.getElementById("videosLink");
-	viLink.className = "";
+	if ( vi != null ){
+		vi.style.display = "none";
+		viLink = document.getElementById("videosLink");
+		viLink.className = "";
+	}
 }
 
 function muestraInfo(id){
@@ -38,8 +49,12 @@ function muestraInfo(id){
 	resetInfo();
 	e = document.getElementById(id);
 	l = document.getElementById(id + "Link");
-	e.style.display = "block";
-	l.className = "activo";
+	if ( e != null ){
+		e.style.display = "block";
+	}
+	if ( l != null ){
+		l.className = "activo";
+	}
 }
 
 function cambiaTab(e){
@@ -54,17 +69,19 @@ function cambiaTab(e){
 $( document ).ready(function() {
 
 		var papa           = $('.menu'),
+			content        = $('.content'),
 			menu           = $('#menu_segmentos ul'),
 			menu_li        = $('#menu_segmentos ul li'),
 			menu_li_a      = $('#menu_segmentos ul li a'),
+			altura_content = ( $('.menu li').height() * 5 ) + 5,
 			altura_papa    = papa.height(),
-			altura_menu    = altura_papa -  ( $('.aplicacion').height() + 40 );
+			altura_menu    = altura_papa - ( $('.aplicacion').height() + 40 );
 
 		menu.css('height',altura_menu);
+		content.css('height',altura_content);
 
 		var altura_menu_li = menu_li.height() + 'px';
 		menu_li_a.css('line-height', altura_menu_li);
-
 
 		//RESIZE
 		$(window).resize(function(){
@@ -72,6 +89,7 @@ $( document ).ready(function() {
 			var altura_papa    = papa.height(),
 				altura_menu    = altura_papa -  ( $('.aplicacion').height() + 40 );
 				console.log(altura_papa);
+
 			menu.css('height',altura_menu);
 
 			altura_menu_li = menu_li.height() + 'px';
